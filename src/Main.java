10288290b8f3862ea4db943f66343065e98bcbfa
bakai <bakai90@inbox.ru>
2022-09-07 12:22:41 +0600

    HomeWork22

--- src/Main.java
+++ src/Main.java
@@ -11,56 +11,31 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
 
-        try (FileWriter fileWriter = new FileWriter("san.txt")){
-            for (char i = 'A'; i <= 'Z'; i++) {
-                String a=i + ". " + Character.toLowerCase(i)+" \n";
-                fileWriter.append(a);
-            }
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        ArrayList<Integer> arrayList1 = new ArrayList<>();
+        ArrayList<Integer> arrayList2 = new ArrayList<>();
 
-            for (char i = '0'; i <='9' ; i++) {
-                fileWriter.append(i+"\n");
+        Random random = new Random();
+        for (int i = 0; i<50; i++){
+            int a = random.nextInt(1,100);
+            arrayList.add(a);
+
+            if(a%2==0){
+                arrayList1.add(a);
+
+            }else {
+                arrayList2.add(a);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
-        try (FileReader fileReader=new FileReader("san.txt")){
-            Scanner scanner=new Scanner(fileReader);
-            int i=1;
-            while (scanner.hasNextLine()){
-                System.out.println(i+") "+scanner.nextLine());
-                i++;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-//        ArrayList<Integer> arrayList = new ArrayList<>();
-//        ArrayList<Integer> arrayList1 = new ArrayList<>();
-//        ArrayList<Integer> arrayList2 = new ArrayList<>();
-//
-//        Random random = new Random();
-//        for (int i = 0; i<50; i++){
-//            int a = random.nextInt(1,100);
-//            arrayList.add(a);
-//
-//            if(a%2==0){
-//                arrayList1.add(a);
-//
-//            }else {
-//                arrayList2.add(a);
-//            }
-//        }
-//        System.out.println("Baardyk sandar" + arrayList);
-//        System.out.println("Jup sandar" + arrayList1);
-//        System.out.println("Tak sandar" + arrayList2);
+        System.out.println("Baardyk sandar" + arrayList);
+        System.out.println("Jup sandar" + arrayList1);
+        System.out.println("Tak sandar" + arrayList2);
 
 
 
 
 
-    }
+    }}
 
 
 
