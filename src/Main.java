222e8494ee1fa2c5c3d8ab11b4ff2f3489cc77d1
bakai <bakai90@inbox.ru>
2022-09-07 12:26:13 +0600

    HomeWork22

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 
-import java.io.FileReader;
-import java.io.FileWriter;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.Scanner;
+
 
 
 public class Main {
